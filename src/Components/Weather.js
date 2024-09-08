1e4eb085a9cd91ce12c85e00ef3e5324ea7caccd
Khursheed Abbas <mirzakhursheedabbas14@gmail.com>
2024-09-08 21:43:05 +0530

    Mycommit

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,4 +1,4 @@
-// import React, { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import Atmosphere from './Atmosphere'
 import Wind from './Wind'
 import Humidity from './Humidity'
